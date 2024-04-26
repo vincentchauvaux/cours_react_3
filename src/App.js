4dd23d68735f1969d4f104d3c51bc7315531c321
Vincent <vincent.chauvaux@gmail.com>
2024-04-26 15:28:18 +0200

    cour react 3 api bouffe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Card from "./components/Card";
 
-function App() {
+const App = () => {
+  const [mealsData, setMealsData] = useState([]);
+  const [inputSearch, setInputSearch] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://www.themealdb.com/api/json/v1/1/search.php?s=" + inputSearch
+      )
+      .then((res) => setMealsData(res.data.meals));
+  }, [inputSearch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1>React Cooking App</h1>
+      <input
+        type="text"
+        placeholder="Taper le nom d'un aliment"
+        onChange={(e) => setInputSearch(e.target.value)}
+      />
+      <div className="meals-container">
+        {mealsData &&
+          mealsData
+            .slice(0, 24)
+            .map((meal) => <Card key={meal.idMeal} meal={meal} />)}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Plats from "../components/Plats";
+
+const Home = () => {
+  return (
+    <div>
+      <Navigation />
+      <Plats />
+    </div>
+  );
+};
+
+export default Home;
